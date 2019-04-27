3be0696b23f94751a3b85d8db79a005e6a303082
WubbaDuck <kd8gby@gmail.com>
2019-04-27 16:28:55 -0400

    Health bars working

---
+++ Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarManager : MonoBehaviour
+{
+    HealthManager topVampHealthManager;
+    HealthManager bottomVampHealthManager;
+    Transform topHealthBarFillTransform;
+    Transform bottomHealthBarFillTransform;
+
+    void Start()
+    {
+        topVampHealthManager = transform.parent.GetChild(0).GetComponent<HealthManager>();
+        bottomVampHealthManager = transform.parent.GetChild(1).GetComponent<HealthManager>();
+        topHealthBarFillTransform = transform.GetChild(0).GetChild(0);
+        bottomHealthBarFillTransform = transform.GetChild(1).GetChild(0);
+
+    }
+
+    void Update()
+    {
+        topHealthBarFillTransform.localScale = new Vector3(((float) topVampHealthManager.GetHealth()) / 100f, 1, 1);
+        bottomHealthBarFillTransform.localScale = new Vector3(((float) bottomVampHealthManager.GetHealth()) / 100f, 1, 1);
+    }
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    private int maxHealth = 100;
+    private int health = 50;
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public bool GiveHealth(int amount)
+    {
+        if ((health + amount) < maxHealth)
+        {
+            health += amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.LogError("Death Not Implemented");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
