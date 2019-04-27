182c91467c8f5dc2a176199e9d43876ecec67489
WubbaDuck <kd8gby@gmail.com>
2019-04-27 13:38:13 -0400

    Added exit door and a game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void PauseGame()
+    {
+
+    }
+
+    // void QuitGame()
+    // {
+    //     System.Diagnostics.Process.GetCurrentProcess().Kill();
+    // }
+}
