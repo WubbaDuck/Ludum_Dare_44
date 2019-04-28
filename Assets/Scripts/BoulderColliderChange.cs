24ac9e3a73035396d4c42329979f1c9ad0a9accd
WubbaDuck <kd8gby@gmail.com>
2019-04-27 23:48:31 -0400

    Added the boulder hazard

---
+++ Assets/Scripts/BoulderColliderChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderColliderChange : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.name.Contains("Border"))
+        {
+            CircleCollider2D col = gameObject.GetComponent<CircleCollider2D>();
+            col.isTrigger = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/BoulderThrowTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderThrowTrigger : MonoBehaviour
+{
+    private BoxCollider2D triggerZone;
+
+    void Start()
+    {
+        triggerZone = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name.Contains("Vampire"))
+        {
+            Rigidbody2D rb = gameObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody2D>();
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.AddForce(new Vector2(18000, 0));
+            Destroy(gameObject.GetComponent<BoxCollider2D>());
+        }
+    }
+
+}
